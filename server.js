3c67fc981f139a066900edfc902a4b868598c63f
GAMELEIRA <gameleira270499@gmail.com>
2020-04-14 00:30:39 -0300

    Adicionado a dependencia express. Configurado o repositório para ser considerado um projeto node com package.json.

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const http = require('http');
 
 const servidor = http.createServer((requisicao, resposta) => {
 
-    resposta.end('<h1>GABRIEL GAMELEIRA DOS SANTOS! O MAIS BRABO!<h1>');
+    let html = '';
+
+    if (requisicao.url == '/') {
+
+        html = '<h1>GABRIEL GAMELEIRA DOS SANTOS! O MAIS BRABO!<h1>';
+
+    } else if (requisicao.url == '/livros') {
+
+        html = '<h1> GAMESGAMES! O MAIS BRABO!<h1>';
+
+    }
+
+    resposta.end(html);
 
 });
 
